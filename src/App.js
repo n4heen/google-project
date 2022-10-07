0d68c496bdbc09a81c5e3665cc1cc602112b109e
Naheen Habib <naheenhabib@outlook.com>
2022-10-07 23:27:01 +1100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 import Home from './pages/Home';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+
 function App() {
   return (
+
+
+
     <div className="app">
-      <h1> Test</h1>
 
-      <Home />
 
+
+      <Router>
+        <Switch>
+          <Route path='/search'>
+            <h1>This is the search page</h1>
+          </Route>
+          <Route path='/' component={Home}/>
+           
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,14 @@ import './Home.css'
 const Home = () => {
     return (
         <div className='home'>
-       
+           <h1>Egg</h1>
             <div className="home__header">
+                <div className="home__headerLeft">
 
+                </div>
+                <div className="home__headerRight">
+
+                </div>
             </div>
             <div className="home__body">
 
