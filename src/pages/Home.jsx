d798f6b323fb9e7b8a45bf3b06bdd1146d177475
Naheen Habib <naheenhabib@outlook.com>
2022-10-08 18:52:16 +1100

    new commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,12 @@ const Home = () => {
             <h1>Egg</h1>
             <div className="home__header">
                 <div className="home__headerLeft">
-                    <Link to='about'>About</Link>
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
                 </div>
                 <div className="home__headerRight">
-
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Images</Link>
                 </div>
             </div>
             <div className="home__body">
