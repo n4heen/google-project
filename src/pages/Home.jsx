62269a4db004034098c910ae074597ab320500d8
Naheen Habib <naheenhabib@outlook.com>
2022-10-08 22:27:28 +1100

    new commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import { Link } from 'react-router-dom'
 import AppsIcon from '@mui/icons-material/Apps';
 import { Avatar } from '@mui/material';
+import Search from '../components/Search';
 
 const Home = () => {
     return (
@@ -15,12 +16,15 @@ const Home = () => {
                 <div className="home__headerRight">
                     <Link to='/gmail'>Gmail</Link>
                     <Link to='/images'>Images</Link>
-                    <AppsIcon/>
-                    <Avatar/>
+                    <AppsIcon />
+                    <Avatar />
                 </div>
             </div>
             <div className="home__body">
-
+                <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="" />
+                <div className="home__inputContainer">
+                    <Search />
+                </div>
             </div>
         </div>
     );
