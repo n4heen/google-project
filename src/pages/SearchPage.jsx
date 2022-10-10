11424c183ce7ca18ea76cd356ee1430293f140ad
Naheen Habib <naheenhabib@outlook.com>
2022-10-10 21:33:27 +1100

    new commit

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 import './SearchPage.css'
 import { useStateValue } from '../StateProvider'
 import UseGoogleSearch from '../useGoogleSearch';
+import response from '../response';
+import { Link } from 'react-router-dom';
+import Search from '../components/Search';
+
 const SearchPage = () => {
 
     const [{ term }, dispatch] = useStateValue()
-    const { data } = UseGoogleSearch(term)
+
+    //LIVE API CALL
+    // const { data } = UseGoogleSearch(term)
+
+    const data = response
 
     console.log(data)
 
     return (
         <div className='searchPage'>
             <div className="searchPage__header">
-                <h1>{term}</h1>
+                <Link to="">
+                    <img className='searchPage__logo' src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="" />
+                </Link>
+                <div className="searchPage__headerBody">
+                    <Search hideButtons />
+                </div>
             </div>
             <div className="searchPage__results">
 
