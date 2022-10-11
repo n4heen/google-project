7160ccbce0640a04d7aa23116388816e66c49a9b
Naheen Habib <naheenhabib@outlook.com>
2022-10-11 21:15:19 +1100

    new commit

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -5,6 +5,12 @@ import UseGoogleSearch from '../useGoogleSearch';
 import response from '../response';
 import { Link } from 'react-router-dom';
 import Search from '../components/Search';
+import SearchIcon from '@mui/icons-material/Search';
+import DescriptionIcon from '@mui/icons-material/Description';
+import ImageIcon from '@mui/icons-material/Image';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import RoomIcon from '@mui/icons-material/Room';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const SearchPage = () => {
 
@@ -27,17 +33,50 @@ const SearchPage = () => {
                     <Search hideButtons />
                     <div className="searchPage__options">
                         <div className="searchPage__optionsLeft">
-
+                            <div className="searchPage__option">
+                                <SearchIcon />
+                                <Link to="/all">All</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <DescriptionIcon />
+                                <Link to="/news">News</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <ImageIcon />
+                                <Link to="/images">Images</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <LocalOfferIcon />
+                                <Link to="/shopping">shopping</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <RoomIcon />
+                                <Link to="/maps">maps</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <MoreVertIcon />
+                                <Link to="/more">more</Link>
+                            </div>
                         </div>
                         <div className="searchPage__optionsRight">
+                            <div className="searchPage__option">
+                                <Link to="/setting">Settings</Link>
+                            </div>
+                            <div className="searchPage__option">
 
+                                <Link to="/tools">Tools</Link>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="searchPage__results">
-
-            </div>
+            {term && (
+                <div className="searchPage__results">
+                    <p className='searchPage__resultCount'>
+                        About {data?.searchInformation.formattedTotalResults} Results ({data?.searchInformation.formattedSearchTime} seconds) for Tesla
+                    </p>
+                </div>
+            )}
         </div>
     );
 }
